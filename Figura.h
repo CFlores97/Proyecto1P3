576249e8cf2fbc4b2eb62b2ed5a6d41b08373424
Carlos Flores <carlosfernandoflores321@gmail.com>
2023-12-06 20:03:46 -0600

    xd

---
+++ Figura.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Figura
+{
+public:
+	virtual double area();
+	virtual double perimetro();
+	virtual void dibujar();
+};
+
